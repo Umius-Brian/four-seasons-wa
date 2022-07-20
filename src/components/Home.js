33c81d0d4609dcdd88c0c94d4fe031bbb3e40756
Brian Bui <umius.brian@gmail.com>
2022-07-20 19:41:23 -0400

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <Link to='/NPS'>
         <Button
           size='lg'
-          style={{ position: 'absolute', top: '10px', right: '300px' }}
+          style={{ position: 'absolute', top: '100px', right: '280px' }}
           variant='dark'
           className='button'
         >
